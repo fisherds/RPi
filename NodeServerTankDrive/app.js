fe8b5f7ca12085ad17fb8ab43af8066134795688
David Fisher <fisherds@rose-hulman.edu>
2021-02-22 10:52:30 -0600

    backend api communication

--- NodeServerTankDrive/app.js
+++ NodeServerTankDrive/app.js
@@ -7,7 +7,7 @@ app.use('/', express.static("public"));
 app.use('/api/', bodyParser.json());
 
 // Drive
-app.get("/api/go/:leftSpeed/:rightSpeed", function (req, res) {
+app.get("/api/motor/go/:leftSpeed/:rightSpeed", function (req, res) {
     let leftSpeed = parseInt(req.params.leftSpeed);
     let rightSpeed = parseInt(req.params.rightSpeed);
     res.json({
@@ -16,37 +16,28 @@ app.get("/api/go/:leftSpeed/:rightSpeed", function (req, res) {
         "leftSpeed": leftSpeed,
     });
 });
-app.get("/api/stop", function (req, res) {
+app.get("/api/motor/stop", function (req, res) {
     res.json({
         "status": "ok"
     });
 });
 
 // Servos
-app.post("/api/armservos", function (req, res) {
+app.post("/api/servo/arm", function (req, res) {
     let angles = req.body.angles;
     res.json({
         "status": "ok",
         "stop": angles,
     });
 });
-app.get("/api/armservo/:jointIndex/:angle", function (req, res) {
-    let jointIndex = parseInt(req.params.jointIndex);
-    let angle = parseInt(req.params.angle);
-    res.json({
-        "status": "ok",
-        "jointIndex": jointIndex,
-        "angle": angle,
-    });
-});
-app.get("/api/gripper/:distanceInches", function (req, res) {
-    let distanceInches = parseInt(req.params.distanceInches);
+app.get("/api/servo/gripper/:distanceInches", function (req, res) {
+    let distanceInches = parseFloat(req.params.distanceInches);
     res.json({
         "status": "ok",
         "distanceinches": distanceInches,
     });
 });
-app.get("/api/camera/:tiltAngle", function (req, res) {
+app.get("/api/servo/camera/:tiltAngle", function (req, res) {
     let tiltAngle = parseInt(req.params.tiltAngle);
     res.json({
         "status": "ok",
@@ -55,7 +46,7 @@ app.get("/api/camera/:tiltAngle", function (req, res) {
 });
 
 // Sensors
-app.get("/api/ultrasonic", function (req, res) {
+app.get("/api/sensor/ultrasonic", function (req, res) {
     // TODO: Get the distance
     const distanceInches = 12;
     res.json({
@@ -63,7 +54,7 @@ app.get("/api/ultrasonic", function (req, res) {
         "distanceInches": distanceInches,
     });
 });
-app.get("/api/reflective", function (req, res) {
+app.get("/api/sensor/reflective", function (req, res) {
     // TODO: Get the reflective sensor values
     const reflectiveValues = 12;
     res.json({
