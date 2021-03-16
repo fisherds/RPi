677c9f480924989403fa230a2bb8c8f84fb98124
David Fisher <fisherds@rose-hulman.edu>
2021-03-16 09:37:40 -0400

    servo kit demo

--- InitialLearning/PythonLearning/servos/adafruit_servokit_test.py
+++ InitialLearning/PythonLearning/servos/adafruit_servokit_test.py
@@ -5,23 +5,26 @@ import adafruit_servokit
 # https://learn.adafruit.com/adafruit-16-channel-servo-driver-with-raspberry-pi/using-the-adafruit-library
 
 def servo_sweep(kit, servo_pin):
-    for k in range(3):
+    for _ in range(2):
         kit.servo[servo_pin].angle = 180
         print("180")
-        time.sleep(2)
+        time.sleep(1.5)
 
         kit.servo[servo_pin].angle = 0
         print("0")
-        time.sleep(2)
+        time.sleep(1.5)
 
 
-def servo_angle(kit, servo_pin)
+def servo_angle(kit, servo_pin):
     while True:
-        angle = input("Enter a servo angle (0-180): ")
+        angle = int(input("Enter a servo angle (0-180): "))
+        if angle == -1:
+            break
         kit.servo[servo_pin].angle = angle
 
 
 def main():
+    print("Ready")
     kit = adafruit_servokit.ServoKit(channels=16)
     servo_pin = 13
     servo_sweep(kit, servo_pin)
