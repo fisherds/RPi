228122986726574b8d40923df818644fb2caf38a
David Fisher <fisherds@rose-hulman.edu>
2021-03-17 12:31:06 -0400

    latest led attempts

--- InitialLearning/NodeLearning/GPIO/leds.js
+++ InitialLearning/NodeLearning/GPIO/leds.js
@@ -1,17 +1,27 @@
-const Gpio = require('pigpio').Gpio;
+const pigpio = require('pigpio');
+
+function msleep(n) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
+
+function sleep(n) {
+    msleep(n * 1000);
+}
 
 function main() {
     console.log("Ready");
+    // pigpio.configureSocketPort(8889);
     basicLedOn();
+    console.log("Goodbye");
 }
 
 function basicLedOn() {
-    redLed = new Gpio(14, {
-        mode: Gpio.OUTPUT
+    redLed = new pigpio.Gpio(14, {
+        mode: pigpio.Gpio.OUTPUT
     });
-    
+
     redLed.digitalWrite(0);
-    
+
 
 }
 
