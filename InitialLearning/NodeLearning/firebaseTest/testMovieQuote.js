fe5c054835362fc68498fabf1c5829131efd8b32
David Fisher <fisherds@rose-hulman.edu>
2021-04-27 09:00:39 -0400

    firebase node server for movie quotes

---
+++ InitialLearning/NodeLearning/firebaseTest/testMovieQuote.js
@@ -0,0 +1,19 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fisherds-movie-quotes-571d2.firebaseio.com"
+});
+
+const db = admin.firestore();
+const ref = db.collection("MovieQuotes");
+
+const observer = ref.onSnapshot(querySnapshot => {
+    console.log(`Received query snapshot of size ${querySnapshot.size}`);
+    querySnapshot.docs.forEach((doc, index) => {
+        console.log(doc.data());
+    })
+  }, err => {
+    console.log(`Encountered error: ${err}`);
+  });
