6298541484e8f047b3681f7add7593287e3ee1fa
David Fisher <fisherds@rose-hulman.edu>
2021-04-27 09:24:48 -0400

    firebase commands

---
+++ Node/NodeFirestoreDrive/tank/app.js
@@ -0,0 +1,35 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://fisherds-movie-quotes-571d2.firebaseio.com"
+});
+
+const db = admin.firestore();
+const ref = db.collection("Commands").doc("command");
+
+ref.onSnapshot(docSnapshot => {
+    if (docSnapshot.exists) {
+        console.log("Snapshot data: ", docSnapshot.data());
+        const message_type = docSnapshot.get("type");
+        console.log("message_type: ", message_type);
+
+        let payload = docSnapshot.get("payload");
+        if (payload) {
+            payload = JSON.parse(payload);
+        }
+        console.log("payload", payload);
+        
+        if (message_type == "motor/go") {
+            const left_wheel_speed = payload[0];
+            const right_wheel_speed = payload[1];
+            console.log("Motor go @ ", left_wheel_speed, right_wheel_speed);
+        } else if (message_type == "motor/stop") {
+            console.log("Motor Stop");
+        }
+    }
+
+}, err => {
+    console.log(`Encountered error: ${err}`);
+});
